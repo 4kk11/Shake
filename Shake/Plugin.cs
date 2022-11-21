45b56fbd49a08158de016ec91bea59c60324cdef
Akihito Yokota <akiaki118row@yahoo.co.jp>
2022-11-21 23:37:36 +0900

    first commit

---
+++ Shake/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using Grasshopper;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace Shake
+{
+	public class Plugin : GH_AssemblyInfo
+	{
+		public override string Name => "Shake";
+		public override Bitmap Icon => null;
+		public override string Description => "";
+		public override Guid Id => new Guid("92389480-2171-47AF-A0CC-55EC1B31D34B");
+		public override string AuthorName => "";
+		public override string AuthorContact => "";
+	}
+
+	public class Priority : GH_AssemblyPriority
+	{
+		public override GH_LoadingInstruction PriorityLoad()
+		{
+			Instances.CanvasCreated += Append__Interaction;
+			return GH_LoadingInstruction.Proceed;
+		}
+
+		private void Append__Interaction(GH_Canvas canvas)
+		{ 
+			Instances.CanvasCreated -= Append__Interaction;
+			//Instances.ActiveCanvas.MouseMove += Shake.Canvas_MouseMove;
+			var shake = new Shake(Instances.ActiveCanvas);
+			//Instances.DocumentEditor.MouseDown += Shake.Canvas_MouseDown;
+			//Instances.DocumentEditor.MouseUp += Shake.Canvas_MouseUp;
+		}
+
+	}
+}
---
+++ Shake/Shake.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.GUI.Canvas.Interaction;
+using Rhino;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shake
+{
+	public class Shake
+	{
+		private System.Threading.Timer timer = null;
+
+		private int count = 0;
+		private Point lastMouseLocation;
+		private Vector lastVec;
+		private Stack<MovementInfo> movementHistory;
+
+		private bool isActiveShakeMode;
+
+		private GH_Canvas canvas;
+		public Shake(GH_Canvas _canvas)
+		{
+			this.canvas = _canvas;
+			canvas.MouseMove += Canvas_MouseMove;
+			canvas.MouseDown += Canvas_MouseDown;
+			canvas.MouseUp += Canvas_MouseUp;
+
+
+		}
+
+		private void Canvas_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (isActiveShakeMode)
+			{
+				//RhinoApp.WriteLine(e.Location.ToString());
+				//int vecX = e.Location.X - lastMouseLocation.X;
+				//int vecY = e.Location.Y - lastMouseLocation.Y;
+				//if (vecX == 0 || vecY == 0) return;
+				//Vector currentVec = new Vector() { X = vecX, Y = vecY };
+
+				Vector currentVec = new Vector(lastMouseLocation, e.Location);
+				if (movementHistory.Count > 0)
+				{
+					if (currentVec.signX() == lastVec.signX() && currentVec.signY() == lastVec.signY())
+					{
+						lastVec.X += currentVec.X;
+						lastVec.Y += currentVec.Y;
+					}
+					else
+					{
+						RhinoApp.WriteLine("changed movement {0}", count++);
+						//RhinoApp.WriteLine("vecX = {0}, vecY = {1}", currentVec.X, currentVec.Y);
+						movementHistory.Push(new MovementInfo() { diff = lastVec, tick = count });
+					}
+				}
+				else
+				{
+					movementHistory.Push(new MovementInfo() { diff = lastVec, tick = count });
+				}
+
+				//RhinoApp.WriteLine("vecX = {0}, vecY = {1}", vecX, vecY);
+
+				lastMouseLocation = e.Location;
+			}
+
+		}
+
+		private void StartShakeMode()
+		{
+			movementHistory = new Stack<MovementInfo>();
+			isActiveShakeMode = true;
+			/*
+			RhinoApp.WriteLine("mouseDown");
+			TimerCallback timerDelegate = new TimerCallback(MyClock);
+			timer = new System.Threading.Timer(timerDelegate, null, 0, 100);
+			//timer.Tick += TickTimer;
+			//timer.Start();
+			*/
+		}
+
+		private void EndShakeMode()
+		{
+			movementHistory.Clear();
+			isActiveShakeMode = false;
+
+			count = 0;
+			lastVec.X = 0;
+			lastVec.Y = 0;
+			/*
+			RhinoApp.WriteLine("mouseUp");
+			if (timer != null)
+			{
+				//RhinoApp.WriteLine(count.ToString());
+				//timer.Stop();
+				timer.Change(Timeout.Infinite, Timeout.Infinite);
+				count = 0;
+
+				timer.Dispose();
+				timer = null;
+			}
+			*/
+		}
+
+		private void Canvas_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (canvas.ActiveInteraction is GH_DragInteraction)
+			{
+				StartShakeMode();
+			}
+		}
+
+		private void Canvas_MouseUp(object sender, MouseEventArgs e)
+		{
+
+			if (isActiveShakeMode)
+			{
+				EndShakeMode();
+			}
+		}
+
+		private void TickTimer(object sender, EventArgs e)
+		{
+			count++;
+		}
+
+		private void MyClock(object o)
+		{
+			
+			count++;
+			RhinoApp.WriteLine(count.ToString());
+		}
+	}
+
+	public struct Vector
+	{
+		public int X;
+		public int Y;
+
+		public Vector(Point ptA, Point ptB)
+		{ 
+			X = ptB.X - ptA.X;
+			Y = ptB.X - ptA.X;
+		}
+		public int signX()
+		{
+			return Math.Sign(X);
+		}
+
+		public int signY()
+		{
+			return Math.Sign(Y);
+		}
+
+	}
+
+	public struct MovementInfo
+	{
+		public Vector diff;
+		public int tick;
+	}
+}
